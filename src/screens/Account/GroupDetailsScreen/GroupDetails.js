7e3f198f1c19ff3aab0f0c1a563b26f68dc1e542
Tolulope Aina-Badejo <foluab@gmail.com>
2023-04-23 14:49:15 +0100

    group is working2

--- src/screens/Account/GroupDetailsScreen/GroupDetails.js
+++ src/screens/Account/GroupDetailsScreen/GroupDetails.js
@@ -31,7 +31,7 @@ export default function GroupDetails({route, navigation}) {
     setCurrentGroupUser(presentUser);
   }
   const goBack = () => {
-    navigation.navigate('Group');
+    navigation.goBack()
   };
 
 
