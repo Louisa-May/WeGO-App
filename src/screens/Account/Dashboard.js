c77c932fd519d5326ddc93194472ce307832084d
Tolulope Aina-Badejo <foluab@gmail.com>
2023-05-09 08:30:47 +0100

    removed errors

--- src/screens/Account/Dashboard.js
+++ src/screens/Account/Dashboard.js
@@ -286,7 +286,7 @@ const getBalance = () => {
             console.log(item);
             navigation.navigate('tripDetails', {item})
           }}  style={styles.tripCardRow}  >
-            <View style={{ width:'100%',justifyContent:'center',  marginTop:-20, alignItems:'center' }}>
+            <View style={{ width:'100%',justifyContent:'center',  marginTop:-20, alignItems:'center', padding:5 }}>
               <Image
                   source={{ uri: item.image }}
                   style={{ width: '100%', height:200, borderTopLeftRadius:10, borderTopRightRadius:10, resizeMode: 'contain' }}
